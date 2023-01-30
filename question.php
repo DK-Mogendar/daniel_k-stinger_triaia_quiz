37f884a34c9e12961438205bff92330288b060fb
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-01-30 15:38:04 +0100

    Anpassungen Chris

--- question.php
+++ question.php
@@ -19,20 +19,20 @@
 <link rel="stylesheet" href="/assets/css/style.css">  
         <script src="/assets/js/main.js"></script>
 <body>
-<?php 
-    require 'include/db.php';
+    <?php 
+        require 'include/db.php';
 
-    // SQL-Statment formulieren: Alle Daten (ganze Tabelle)
-    //zur Frage mit der  angegebenen $id auslesen
-    $id = 9999;
-    $question = fetchQuestionsById($id, $dbConnection);
-   
- ?> 
+        // SQL-Statment formulieren: Alle Daten (ganze Tabelle)
+        //zur Frage mit der  angegebenen $id auslesen
+        $id = 222;
+        $question = fetchQuestionsById($id, $dbConnection);
+    ?> 
+
     <div class="container-flex"><!--header start--> 
          <?php include './include/header.php' ?>   
     </div><!--header ende-->
 
-    <div class="footer container"><!--include db-->
+
 
     </div>
 
@@ -42,14 +42,14 @@
         <p><?php echo   $question['question_text'];?></p>
     </div>
 
-    <div class="container justify-content-center text-white">
+   <!-- <div class="container justify-content-center text-white">
             <h3 class="px-4 text-start">Frage ansich</h3>
             <br>
         <form class="px-4" action="./report.php" method="post">
           <div class="btn-group flex-wrap" id="button-group" role="group" aria-label="Basic radio toggle button group">
             <input type="radio" class="btn-check" name="btnradio" id="btnradio1" value="1" autocomplete="off" checked>
             <label class="btn btn-outline-primary my-3" for="btnradio1"><?php echo $question['answer-1'];?></label>
-           
+ 
             <input type="radio" class="btn-check" name="btnradio" id="btnradio2" value="2" autocomplete="off">
             <label class="btn btn-outline-primary my-3" for="btnradio2"><?php echo $question['answer-2'];?></label>
 
@@ -62,7 +62,45 @@
             <input type="radio" class="btn-check" name="btnradio" id="btnradio5" value="5" autocomplete="off">
             <label class="btn btn-outline-primary my-3" for="btnradio5"><?php echo $question['answer-5'];?></label>
         </form>
-    </div>
+    </div>-->
+
+
+
+    <form id="quiz-form" action="question.php" method="post" onsubmit="return navigate('next');">
+        <?php 
+
+        //Generiere Antwort.Radio-Bittons mit Beschriftung
+
+        //Singel Choice: Hole den Namen  der richtigen Antwortspalte in $correct, aus $question["correct"]
+        $correct = $question["correct"];
+
+        for ($a = 1; $a <= 5; $a++) {
+            //Setzte für $answerColumnName den Namen der Tabellensplte "answer-N" zusammen
+            $answerColumnName = "answer-" . $a;
+
+            //Falls es überhaubt Antworttext in $question[$answerColumnName]gibt
+            //und der Antworttext nicht gleich '', dann ...
+
+            if (isset($question[$answerColumnName]) && $question[$answerColumnName]  !== '') {
+                //....hole den antworttext aus $question.
+                $answerText = $question[$answerColumnName];
+
+                //Entscheide für $value, wieviele Punkte die Antwort ergiebt:
+                //richtig -> 1 Punkt, falsch -> 0 Punkte
+                if ($correct === $answerColumnName) $value = 1;
+                else $value = 0;
+
+                echo "<div class='form-check'>
+                        <input class='form-check-input' type='radio' name='singel-choice id='$answerColumnName' value='$value'>
+                        <label class='form-check-label' for='single-choice-0'>$answerText</label>
+                    </div>";
+            
+                }
+            }
+        
+        ?>
+    </form>
+
       
     <div class="container-flex"><!--footer Start-->
           <?php include './include/footer2.php'?>  
