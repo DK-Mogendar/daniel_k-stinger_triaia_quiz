fc92fea73fb6efaa5894053231558e0eb914341f
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-02-14 13:51:10 +0100

    zw.

--- index.php
+++ index.php
@@ -14,55 +14,66 @@
     <script src="js/main.js"></script>
 
 <!--Bootstrap 5-->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!--Boostrap Icons-->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 <!--einbindung Javascript und Css Dokument-->
-<link rel="stylesheet" href="/assets/css/style.css">  
-        <script src="/assets/js/main.js"></script>
+    <link rel="stylesheet" href="/assets/css/style.css">  
+    <script src="/assets/js/main.js"></script>
 </head>
+
 <body>
     <div class="container-flex"><!--header start--> 
-         <?php include './include/header.php' ?>   
-      
+         <?php include './include/header.php' ?>     
     </div><!--header ende-->
 
 
+<!-- FORMULAR "Themenwahl"-->
+    <div style="padding: 20px;">
+        <form id="quiz-form" action="question.php" method="post" onsubmit="return navigate('next');"> 
+            <!--Themenauswahl-->
+            <label for="quiz-topic" class="="form-label>Quiz Thema - bitte auswählen</label>
+            <select style="width: 400px;" class="form-select" aria-label="Default select exemple" id="topic" name="topic">
+                <option value="music">Music</option>
+                <option value="ch-norris">Ch Norris</option>
+                <option value="animals">Animals</option>
+                <option value="movies">Movies</option>
+                <option value="d-n-d">D&D</option>
+                <option value="astronautics">Astronautics</option>
+                <option value="technology">Technologyy</option>
+                <option value="ai">AI</option>
+                <option value="geography">Geographys</option>
+                <option value="sports">Sports</option>
+                <option value="science">Science</option>
+                <option value="informatics">Informatics</option>
+                <option value="gen-knowledge"ograp>Gen Knowledge</option>
+                <option value="basketball">Basketball</option>
+            </select>
 
- <div style="padding: 20px;">
- <form id="quiz-topic" action="question.php" method="post" onsubmit="return navigate('next');"> 
-    <!--Themenauswahl-->
-    <label for="quiz-topic" class="="form-label>Quiz Thema - bitte auswählen</label>
-    <select style="width: 400px;" class="form-select" aria-label="Default select exemple" id="topic" name="topic">
-        <option values="geography">Geography</option>
-        <option values="animals">Animals</option>
-        <option values="movies">Movies</option>
-    </select>
+            <!--Anzahl Fragen-->
+            <label style="margin-top:20px;" for="questionNum" class="form-label">Number of Question</label>      
+            <input style="width:100px" type="number" class="form-control" 
+                  id="questionNum" name="questionNum" 
+                  min="2" max="40" value="10">
+            <!--
+              input type="hidden"
+              lastQuestionIndex: mit PHP gesetzt
+              indexStep: mit JavaScript setValue.......
+            -->
+            <input type="hidden" id="lastQuestionIndex" name="lastQuestionIndex" value="-1">
+            <input type="hidden" id="indexStep" name="indexStep" value="1">
 
-    <!--Anzahl Fragen-->
+            <p id=" validation-warning" class="warnig"></p>
 
-
-
-    <label style="margin-top:20px;" for="questionNum" class="form-label">Number of Question</label>      
-    <input style="width:100px" type="number" class="form-control" id="questionNum" name="questionNum" min="5" max="40" value="10">
-      <!--
-        input type="hidden"
-        lastQuestionIndex: mit PHP gesetzt
-        indexStep: mit JavaScript setValue.......
-      -->
-    <input type="hidden" id="lastQuestionIndex" name="lastQuestionIndex" value="-1">
-    <input type="hidden" id="indexStep" name="indexStep" value="1">
-
-    <p id=" validation-warning" class="warnig"></p>
-
-    
-    <!--submitbutton-->
-    <button type="submit" class="btn btn-primary">Start</button>
-</form>
-
-    <!-- <div class="footer container">    footer Start-->
-          <?php //include './include/footer.php'?>  
+            
+            <!--submitbutton-->
+            <input style="margin-top:20px;" type="submit" value="Start">
+        </form>
+    </div>
+<!--<div class="footer container">    footer Start-->
+        <?php //include './include/footer.php'?>  
     </div><!--footer Ende-->
+
 </body>
 </html>
--- question.php
+++ question.php
@@ -13,91 +13,95 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/main.js"></script>
-</head>
+
 <!--Bootstrap 5-->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!--Boostrap Icons-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 <!--einbindung Javascript und Css Dokument-->
-<link rel="stylesheet" href="/assets/css/style.css">  
-        <script src="/assets/js/main.js"></script>
+    <link rel="stylesheet" href="/assets/css/style.css">  
+    <script src="/assets/js/main.js"></script>
+</head>
 <body>
     <?php 
-        // SQL-Statment formulieren: Alle Daten (ganze Tabelle)
-        //zur Frage mit der  angegebenen $id auslesen
-        $id = 222;
-        $question = fetchQuestionsById($id, $dbConnection);
-    ?> 
-<!--hidden ist ein verstecktes Feld das nich angezeigt wird-->
-<input type="hidden" id="questionNum" value="15">
-<input type="hidden" id="lastQuestionIndex" name="lastQuestionIndex" value="11">
-<input type="hidden" id="indexStep" name="indexStep" value="1">
+        // Bestimme die Anzahl der verfügbaren Fragen
+        if (isset($quiz["questionIdSequence"])) {
+            $id = $quiz["questionIdSequence"][$currentQuestionIndex];
+        } 
 
+        // Hole alle Datenfelder zur Frage mit $id von der Datenbank
+        $question = fetchQuestionById($id, $dbConnection);
+    ?> 
     <div class="container-flex"><!--header start--> 
-         <?php include './include/header.php' ?>   
+        <?php include './include/header.php' ?>   
     </div><!--header ende-->
 
+    <!--FORMULAR "Fragestellung"-->
+    <div class="row" STYLE="padding: 20px;">
+        <div class="colsm-8">
+            <!--Fragestellung-->
+            <h7>Frage<?php echo ($currentQuestionIndex +1); ?> von <?php echo $quiz["question"];?></h7>
+            <p>&nbsp;</p>
+            <h3><?php echo $question["question_text"];?>/h3>
+            <p>&nbsp;</p>
+
+            <form id="quiz-form" action="<?php echo $actionUrl; ?>" method="post" onsubmit="return navigate('next');">
+                <?php 
+                    // Generelle Antwort_Radio_Buttons mit Beschriftung
+
+                    // Single Choice: Hole den Namen der richtigen Antwortspalte in $correct, aus $question["correct"]
+                    $correct = $question["correct"];
+
+                    for ($a =1; $a <= 5; $a++) {
+                        // Setze für $answerColumnName den Namen der Tabellenspalte "answer-N" zusammen
+                         $answerColumnName = "answer-" . $a;
 
 
-    <div class="titelbox container d-flex justify-content-center p5 text-white bg-primary "> 
-        <h1 class="titel col-4">Questions</h1>
-    
-        <p class="titel col-4"><?php echo   $question['question_text'];?></p>
-    </div>
+                        // Falls es überhaupt Antworttext in $question[$answerColumnName] gibt
+                        // und der Antwortext nicht gleich '', dann ...
+                        if (isset($question[$answerColumnName]) && $question[$answerColumnName] !== '') {
+                            // Holt den Antworttext aus $question.
+                            $answerText = $question[$answerColumnName];
 
+                            // Entscheide für $value, wieviele Punkte die Anwort ergibt: 
+                            // richtig -> 1 Punkt, falsch -> 0 Punkte
+                            if ($correct === $answerColumnName) $value = 1;
+                            else $value = 0;
 
-    <div class="container d-flex col-12 pt-5 text-dark">
-
-        <img class="container-flex col-4" src="/assets/img/tier.png" alt="test">
-      
-        <form class="frage p-5 container-flex col-4"  id="quiz-form" action="question.php" method="post" onsubmit="return navigate('next');">
-            <?php 
-
-            //Generiere Antwort.Radio-Bittons mit Beschriftung
-
-            //Singel Choice: Hole den Namen  der richtigen Antwortspalte in $correct, aus $question["correct"]
-            $correct = $question["correct"];
-
-            for ($a = 1; $a <= 5; $a++) {
-                //Setzte für $answerColumnName den Namen der Tabellensplte "answer-N" zusammen
-                $answerColumnName = "answer-" . $a;
-
-                //Falls es überhaubt Antworttext in $question[$answerColumnName]gibt
-                //und der Antworttext nicht gleich '', dann ...
-
-                if (isset($question[$answerColumnName]) && $question[$answerColumnName]  !== '') {
-                    //....hole den antworttext aus $question.
-                    $answerText = $question[$answerColumnName];
-
-                    //Entscheide für $value, wieviele Punkte die Antwort ergiebt:
-                    //richtig -> 1 Punkt, falsch -> 0 Punkte
-                    if ($correct === $answerColumnName) $value = 1;
-                    else $value = 0;
-                    
-
-
-                    echo "<div class='form-check'>
-                            <input class='form-check-input' type='radio' name='singel-choice id='$answerColumnName' value='$value'>
-                            <label class='form-check-label' for='single-choice-0'>$answerText</label>
-                        </div>";
+                            echo "<div class='form-check'>
+                                    <input class='form-check-input' type='radio' name='single-choice' id='$answerColumnName' value='$value'>
+                                    <label class='form-check-label' for='$answerColumnName'>$answerText</label>
+                                  </div>";
+                        }
                     }
-                }
-            ?><!--submitbutton-->
-             <button type="submit" class="btn btn-primary">Weiter</button>
-        </form>
-        
-        <img class="container col-4" src="/assets/img/tier.png" alt="test">
-    
+
+                ?>
+
+                <!--input type="hidden"
+                    questionNum, lastQuestionIndex: mit PHP gesetzt
+                    indexStep: mit JavaScript setIntValue(fieldId, value) verändert
+                -->
+
+                <input type="hidden" id="questionNum" value="<?php echo $quiz["questionNum"]; ?>">
+                <input type="hidden" id="lastQuestionIndex" name="lastQuestionIndex" value="<?php echo $currentQuestionIndex; ?> ">
+                <input type="hidden" id="indexStep" name="indexStep" value="1">
+
+                <!-- Validierungswarnung -->
+                <p id="validation-warning" class="warning"></p>
+
+                <!-- submit -->
+                <!-- button type="submit" class="btn btn-primary" onclick="navigatePrevious();">Previous</button -->
+                <button type="submit" class="btn btn-primary" 
+                        style="position:fixed;bottom:150px;">Next</button>
+                <p class="spacer"></p>
+            </form>
+        </div>
     </div>
-
-    <!--Mein Button um zurück zur Startseite zu kommen-->
-    <button class="buton" type="buton" class= "btn btn-info " onclick="document.location='index.php';">
-            Zurück zum Anfang
-            <span style='font-size:50px;'>&#128512;</span>
-
+            
     <div class="container-fluid"><!--footer Start-->
           <?php //include './include/footer2.php'?>  
     </div><!--footer Ende-->
+
 </body>
 </html>
